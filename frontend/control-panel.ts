3f373bbd08f3c0940d6918ed44ee965ac83508d9
Łukasz Cieśnik <lukasz.ciesnik@gmail.com>
2025-12-27 00:41:47 +0100

    frontend code small reorg

--- frontend/control-panel.ts
+++ frontend/control-panel.ts
@@ -1,36 +1,98 @@
 
 import '@material/web/all.js'; // @todo minimize imports
-import '@material/web/icon/icon';
-
 import './mailing-lists';
-
 import { LitElement, html } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, property, state } from 'lit/decorators.js';
 import { provide } from '@lit/context';
 import { Task } from '@lit/task';
 import { msg } from '@lit/localize';
+import { MdSwitch } from '@material/web/switch/switch';
+import { query } from 'lit/decorators/query.js';
+import { queryAll } from 'lit/decorators/query-all.js';
+import { repeat } from 'lit/directives/repeat.js';
 import { apiRoot, tokenContext } from './context';
-import { SubscriptionStatus } from '../backend/subscription';
 import { initializeLocale } from './localize';
+import { ListSubscription } from './mailing-lists';
+import type { SubscriptionStatus, UpdateSubscriptionRequest } from '../backend/subscription';
 
-const query = new URLSearchParams(window.location.search);
 function getToken(): string | undefined {
-  const token = query.get('token');
+  const queryParams = new URLSearchParams(window.location.search);
+  const token = queryParams.get('token');
   if (token === null) {
     return undefined;
   } else {
     return token;
   }
 }
+
 @customElement('mailer-control-panel')
 export class ControlPanel extends LitElement {
 
-  @provide({context: tokenContext})
-  token = getToken();
+  @property({type: Boolean})
+  public autosubscribe?: boolean = true;
 
-  async connectedCallback() {
+  @provide({context: tokenContext})
+  protected token = getToken();
+
+  @state()
+  protected data?: SubscriptionStatus;
+
+  @query('#subscribe')
+  private subscribeSwitch?: MdSwitch;
+
+  @queryAll('mailer-list-subscription')
+  private mailingListItems?: NodeListOf<ListSubscription>;
+
+  public async connectedCallback() {
     super.connectedCallback();
     await initializeLocale();
+  }
+
+  // @todo update name?
+
+  // @todo autosubscribe
+  // https://lit.dev/docs/components/events/#adding-event-listeners-to-other-elements
+
+  protected render() {
+    return this.fetchSubscriptionTask.render({
+      pending: () => html`<md-circular-progress four-color indeterminate></md-circular-progress>`,
+      complete: (status) => {
+        return this.renderSubscriptionStatus(status);
+      },
+      error: (error) => html`<md-suggestion-chip><md-icon slot="icon">error</md-icon>${String(error)}</md-suggestion-chip>`
+    });
+  }
+
+  protected renderSubscriptionStatus(subscription: SubscriptionStatus) {
+    const data = subscription;
+    const status = this.updateSubscriptionTask.render({
+      pending: () => html`<md-linear-progress indeterminate></md-linear-progress>`,
+      complete: () => html``,
+      error: (error) => html`<md-suggestion-chip><md-icon slot="icon">error</md-icon>${String(error)}</md-suggestion-chip>`
+    })
+
+    // @todo show e-mail, company name
+    // @todo use fab https://material-web.dev/components/fab/ for main subscription
+    // @todo label https://material-web.dev/components/switch/#label
+    return html`
+      <md-list>
+        <md-list-item type="button">
+          <div slot="headline">${msg('Subscribe for newsletter')}</div>
+          <div slot="trailing-supporting-text">
+            <md-switch icons id="subscribe" ?selected=${data.subscribed} @change=${this.onChange}></md-switch>
+          </div>
+        </md-list-item>
+        ${repeat(
+          data.mailingLists,
+          (list) => list.id,
+          (list, index) => html`
+            <mailer-list-subscription .mailingListId=${list.id} .name=${list.name} .description=${list.description}
+              ?subscribed=${list.subscribed} ?disabled=${!data.subscribed}
+              @change=${this.onChange}>
+            </mailer-list-subscription>`
+        )}
+      </md-list>
+      ${status}`;
   }
 
   private fetchSubscriptionTask = new Task(this, {
@@ -46,18 +108,55 @@ export class ControlPanel extends LitElement {
         throw new Error(`HTTP ${response.status}`);
       }
       // @todo handle token refresh
-      return await response.json() as SubscriptionStatus;
+      this.data = await response.json() as SubscriptionStatus;
+      return this.data;
     },
     args: () => [this.token]
   });
+  
+  private updateSubscriptionTask = new Task(this, {
+    task: async ([data, token], {signal}) => {
+      if (token === undefined) {
+        throw new Error(msg('Missing authorization token'));
+      }
+      if (this.data === undefined)
+        return;
+      
+      const email = this.data.email;
+      const subscribe : boolean = this.subscribeSwitch?.selected || false;
+      const mailingLists : Record<string, boolean> = {};
+      this.mailingListItems?.forEach((list) => {
+        mailingLists[list.id] = list.subscribed;
+      })
 
-  render() {
-    return this.fetchSubscriptionTask.render({
-      pending: () => html`<md-circular-progress four-color indeterminate></md-circular-progress>`,
-      complete: (status) => {
-        return html`<mailer-subscription-control .data=${status} ></mailer-subscription-control>`;
-      },
-      error: (error) => html`<md-suggestion-chip><md-icon slot="icon">error</md-icon>${String(error)}</md-suggestion-chip>`
-    });
+      const request : UpdateSubscriptionRequest = {email, subscribe, mailingLists};
+      try {
+        const response = await fetch(`${apiRoot}/subscription`, {
+          method: 'PUT',
+          headers: {
+            'Authorization': `Bearer ${this.token}`,
+            'Accept': 'application/json',
+            'Content-Type': 'application/json; charset=utf-8',
+          },
+          body: JSON.stringify(request)
+        });
+        if (!response.ok) {
+          throw new Error(`HTTP ${response.status}`);
+        }
+
+        this.data.subscribed = subscribe;
+        this.data.mailingLists.forEach((list) => {
+          list.subscribed = mailingLists[list.id];
+        });
+      } catch (error) {
+        this.dispatchEvent(new CustomEvent('error', {detail: {error}}));
+      }
+    },
+    args: () => [this.data, this.token],
+    autoRun: false,
+  });
+
+  private async onChange(e: Event) {
+    this.updateSubscriptionTask.run();
   }
 }
--- frontend/mailing-lists.ts
+++ frontend/mailing-lists.ts
@@ -1,146 +1,33 @@
 
 import { LitElement, html, css } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
-import { query } from 'lit/decorators/query.js';
-import { queryAll } from 'lit/decorators/query-all.js';
-import { repeat } from 'lit/directives/repeat.js';
-import { Task } from '@lit/task';
-import { consume } from '@lit/context';
-import { msg } from '@lit/localize';
 import { MdSwitch } from '@material/web/switch/switch';
-import { apiRoot, tokenContext } from './context';
-import { SubscriptionStatus, UpdateSubscriptionRequest } from '../backend/subscription';
-
-@customElement('mailer-subscription-control')
-export class Subscription extends LitElement {
-  @consume({context: tokenContext})
-  @property({attribute: false})
-  private token?: string;
-
-  @property({
-    attribute: false,
-    hasChanged: (a: any, b: any) => JSON.stringify(a) !== JSON.stringify(b)
-  })
-  public data?: SubscriptionStatus;
-
-  @property({type: Boolean})
-  public autosubscribe?: boolean = true;
-
-  @query('#subscribe')
-  private subscribeSwitch?: MdSwitch;
-
-  @queryAll('mailer-list-subscription')
-  private mailingListItems?: NodeListOf<ListSubscription>;
-
-  // @todo update name?
-
-  // @todo autosubscribe
-  // https://lit.dev/docs/components/events/#adding-event-listeners-to-other-elements
-
-  render() {
-    if (this.data !== undefined) {
-      const data = this.data;
-      const status = this.updateSubscriptionTask.render({
-        pending: () => html`<md-linear-progress indeterminate></md-linear-progress>`,
-        complete: () => html``,
-        error: (error) => html`<md-suggestion-chip><md-icon slot="icon">error</md-icon>${String(error)}</md-suggestion-chip>`
-      })
-
-      // @todo show e-mail, company name
-      // @todo use fab https://material-web.dev/components/fab/ for main subscription
-      // @todo label https://material-web.dev/components/switch/#label
-      return html`
-        <md-list>
-          <md-list-item type="button">
-            <div slot="headline">${msg('Subscribe for newsletter')}</div>
-            <div slot="trailing-supporting-text">
-              <md-switch icons id="subscribe" ?selected=${data.subscribed} @change=${this.onChange}></md-switch>
-            </div>
-          </md-list-item>
-          ${repeat(
-            data.mailingLists,
-            (list) => list.id,
-            (list, index) => html`
-              <mailer-list-subscription id=${list.id} name=${list.name} description=${list.description}
-                ?selected=${list.subscribed} ?disabled=${!data.subscribed}
-                @change=${this.onChange}>
-              </mailer-list-subscription>`
-          )}
-        </md-list>
-        ${status}`;
-    }
-  }
-
-  private updateSubscriptionTask = new Task(this, {
-    task: async ([data, token], {signal}) => {
-      if (token === undefined) {
-        throw new Error(msg('Missing authorization token'));
-      }
-      if (this.data === undefined)
-        return;
-      
-      const email = this.data.email;
-      const subscribe : boolean = this.subscribeSwitch?.selected || false;
-      const mailingLists : Record<string, boolean> = {};
-      this.mailingListItems?.forEach((list) => {
-        mailingLists[list.id] = list.subscribed;
-      })
-
-      const request : UpdateSubscriptionRequest = {email, subscribe, mailingLists};
-      try {
-        const response = await fetch(`${apiRoot}/subscription`, {
-          method: 'PUT',
-          headers: {
-            'Authorization': `Bearer ${this.token}`,
-            'Accept': 'application/json',
-            'Content-Type': 'application/json; charset=utf-8',
-          },
-          body: JSON.stringify(request)
-        });
-        if (!response.ok) {
-          throw new Error(`HTTP ${response.status}`);
-        }
-
-        this.data.subscribed = subscribe;
-        this.data.mailingLists.forEach((list) => {
-          list.subscribed = mailingLists[list.id];
-        });
-      } catch (error) {
-        this.dispatchEvent(new CustomEvent('error', {detail: {error}}));
-      }
-    },
-    args: () => [this.data, this.token],
-    autoRun: false,
-  });
-
-  private async onChange(e: Event) {
-    this.updateSubscriptionTask.run();
-  }
-}
+import '@material/web/icon/icon';
 
 @customElement('mailer-list-subscription')
 export class ListSubscription extends LitElement {
 
-  static properties = {
-    mailingListId: { type: String },
-    name: { type: String },
-    description: { type: String },
-    subscribed: { type: Boolean },
-    disabled: { type: Boolean },
-  };
+  @property({type: String})
+  public mailingListId?: string;
 
-  mailingListId?: string;
-  name?: string;
-  description?: string;
-  subscribed: boolean = false;
-  disabled: boolean = false;
+  @property({type: String})
+  public name?: string;
+
+  @property({type: String})
+  public description?: string;
+
+  @property({type: Boolean})
+  public subscribed: boolean = false;
+
+  @property({type: Boolean})
+  public disabled: boolean = false;
 
   static styles = css`
     .name { font-weight: 600 }
     .description { font-style: italic; } 
   `;
 
-  render(){
+  protected render(){
     return html`
       <md-list-item type="button">
         <md-icon slot="start">label</md-icon>
@@ -160,7 +47,7 @@ export class ListSubscription extends LitElement {
   }
 }
 
-class SubscriptionChangeEvent extends Event {
+export class SubscriptionChangeEvent extends Event {
   subscribe: boolean;
   mailingListId?: string;
 
